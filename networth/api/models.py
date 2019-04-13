18260883d0e005660f1fd0f5663a92bf194dd1be
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-13 22:00:32 +0530

    add Category, Expense, Income

--- networth/api/models.py
+++ networth/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+
+	title = models.CharField(max_length=100)
+	description = models.TextField()
+
+class Expense(models.Model):
+
+	categories = models.ForeignKey(Category, null=True,on_delete = models.SET_NULL)
+	title = models.CharField(max_length=100)
+	description = models.TextField()
+	cost = models.IntegerField(default=0)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+class Income(models.Model):
+
+	categories = models.ForeignKey(Category, null=True,on_delete = models.SET_NULL)
+	title = models.CharField(max_length=100)
+	description = models.TextField()
+	money = models.IntegerField(default=0)
+	timestamp = models.DateTimeField(auto_now_add=True)
