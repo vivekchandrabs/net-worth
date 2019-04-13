de5ef4d6d1fbcb1a93039cc501e6910c20c367f9
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-13 22:03:31 +0530

    install webserver, api, rest_framework

--- networth/networth/settings.py
+++ networth/networth/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'webserver',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
