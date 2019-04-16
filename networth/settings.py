6a6f4e10518765fdc37aad3ef7edcd378fc5598b
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-16 17:48:33 +0530

    Add REST_FRAMEWORK permissions

--- networth/settings.py
+++ networth/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'api',
     'webserver',
     'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -72,6 +73,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'networth.wsgi.application'
 
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+   ),
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
