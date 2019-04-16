b4b401ec727c3d5817cc2f99faa38cd2eaaccaf0
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-16 20:14:43 +0530

    modify nothing

--- api/models.py
+++ api/models.py
@@ -2,16 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-
 class Category(models.Model):
 
 	title = models.CharField(max_length=100)
 	description = models.TextField()
-	user = models.ForeignKey(User, null=True,on_delete=models.SET_NULL)
+	user = models.ForeignKey(User, blank=True,null=True,on_delete=models.SET_NULL)
 
 class Expense(models.Model):
 
-	categories = models.ForeignKey(Category, null=True,on_delete = models.SET_NULL)
+	categories = models.ForeignKey(Category, default=3, null=True,on_delete = models.SET_NULL)
 	title = models.CharField(max_length=100)
 	description = models.TextField()
 	cost = models.IntegerField(default=0)
