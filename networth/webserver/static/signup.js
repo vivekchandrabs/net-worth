b5472175e1b72ef110ab8ec15d48e0290d1b64b2
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-18 18:17:34 +0530

    add signin and signup js files

--- networth/webserver/static/signup.js
+++ networth/webserver/static/signup.js
@@ -1,20 +1,23 @@
-$("signup").on('click', function(){
+$("#signup").on("click", function(){
 	let username = $("#username").val();
-	let email = $("#Email").val();
+	let email = $("#email").val();
 	let password = $("#password").val();
 
-	var data = {};
+	var data = {};     
 	data["username"] = username;
 	data["email"] = email;
 	data["password"] = password;
-
+	console.log(email);
 	$.ajax({
 		method: 'POST',
 		url: 'http://127.0.0.1:8000/api/signup/',
 		type: 'application/json',
 		data: data,
 		success: function(response){
-			console.log("done");
+			window.localStorage.setItem('token', response.token);
+			window.location = '/dashboard/';
 		}
 	}); 
-});
+}); 
+
+
