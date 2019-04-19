37f8f96ff67fb98f97653dfb20193dc915d31bec
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-19 16:53:27 +0530

    Add url to the TableDataViewSet, MonthViewSet, IncomeExpenseViewSet

--- networth/api/urls.py
+++ networth/api/urls.py
@@ -11,6 +11,8 @@ router.register(r'income',IncomeViewSet)
 router.register(r'signup',SignupViewSet, base_name="Signup")
 router.register(r'allmonthexpense',AllMonthViewSet, base_name="AllMonth")
 router.register(r'inexp',IncomeExpenseViewSet,base_name='IncomeExpense')
+router.register(r'monthexp',MonthViewSet,base_name='Month')
+router.register(r'tabledata',TableDataViewSet, base_name="TableData")
 
 urlpatterns = [
 	
