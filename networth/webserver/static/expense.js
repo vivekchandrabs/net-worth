cbcd0da8a8a6de3fefe24705c64967a3c4e44a53
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-20 12:36:28 +0530

    add expense.js

--- networth/webserver/static/expense.js
+++ networth/webserver/static/expense.js
@@ -4,23 +4,49 @@ $("#add").on('click', function(){
 	
 	
 	$("#category").append(`
+		<label for="recipient-name" class="col-form-label">Enter your new category :</label>
 		<input id = ${count} class = "form-control" type='text'>
+		`)
+
+	$("#description").append(`
+		<label for="recipient-name" class="col-form-label">Enter your new description :</label>
+		<input id = ${count+"d"} class = "form-control" type='text'>
 		`)
 	count = count + 1;
 	console.log(count);
-
-
 });
 
 $("#save_category").on("click", function(){
 
 	for(let i=0;i<count;i++)
 	{
+
 		var cat = $("#"+i).val();
-		console.log(cat);
+		var description = $("#"+i+"d").val();
+		let data = {}
+		data["title"] = cat;
+		data["description"] = description;
+		
+		$.ajax({
+
+			method:"POST",
+			url:"/api/category/",
+			type: "application/json",
+			data: data,
+			beforeSend: function(xhr){
+
+			xhr.setRequestHeader("Authorization", "Token " + window.localStorage["token"]);
+			},
+			success: function(response)
+			{
+				console.response(response);
+			}
+
+		});
 	}
 	$('#category').empty()
-})
+
+});
 
 
 $.ajax({
