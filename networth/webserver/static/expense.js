1499a2aca3ea35a03cebe97695ee6f976c175808
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-22 18:30:23 +0530

    add how edit expense and edit income modal

--- networth/webserver/static/expense.js
+++ networth/webserver/static/expense.js
@@ -1,28 +1,33 @@
 var count = 1;
 var checkout = 1;
+var edit_exp_id = 1;
+var edit_inc_id = 1;
+var date = new Date()
+var current_date = date.getMonth() + 1
+var income_date = date.getMonth() + 1
 
-$(document).ready(function(){
-	$.ajax({
+// $(document).ready(function(){
+// 	$.ajax({
 
-		method:"GET",
-		url:"/api/expense/",
-		beforeSend: function(xhr){
-			xhr.setRequestHeader("Authorization", "Token "+ window.localStorage["token"])
-		},
-		success: function(response) {
-			console.log(response);
-			for (let expense of response) {
-				$('#all-expenses').append(`<tr>
-					<td>${expense.title}</td>
-					<td>${expense.description}</td>
-					<td>${expense.cost}</td>
-					<td>${ expense.categories.title}</td>
-					<td><button class="btn btn-sm btn-success edit-expense id="edit">Edit   </button></td>
-				</tr>`)
-			}
-		}
-	});
-});
+// 		method:"GET",
+// 		url:"/api/expense/",
+// 		beforeSend: function(xhr){
+// 			xhr.setRequestHeader("Authorization", "Token "+ window.localStorage["token"])
+// 		},
+// 		success: function(response) {
+// 			console.log(response);
+// 			for (let expense of response) {
+// 				$('#all-expenses').append(`<tr>
+// 					<td>${expense.title}</td>
+// 					<td>${expense.description}</td>
+// 					<td>${expense.cost}</td>
+// 					<td>${ expense.categories.title}</td>
+// 					<td><button class="btn btn-sm btn-success edit-expense id="edit">Edit   </button></td>
+// 				</tr>`)
+// 			}
+// 		}
+// 	});
+// });
 
 $("#add").on('click', function(){
 	
@@ -243,107 +248,215 @@ $("#save_income").on("click", function(){
 
 });
 
-$("#edit_expense").on("click", function(){
+// for the edit expense modal;
+function edit_expense(month_number){
 
-	// let cat_id = $("#select_category").find(":selected").val();
-	// let cost_money = $("#expense_cost").val()
-	// let cost_title = $("#expense_title").val()
-	// let cost_des = $("#expense_des").val()
+	
+	if(current_date + month_number <= 1){
+		current_date = 1;
+	}
+	else if(current_date + month_number >= 12){
+		current_date = 12
+	}
+	else{
 
-	// let json_data = {}
-
-	// json_data["title"] = cost_title
-	// json_data["categories"] = cat_id
-	// json_data["cost"] = cost_money
-	// json_data["description"] = cost_des
-
+		current_date = month_number + current_date
+	}
+	let data = {}
+	data["month"] = 4
+	console.log(data);
+	console.log(current_date);
 	$.ajax({
 
 		method:"GET",
 		url:"/api/expense/",
-		type:"application/json",
+		type:"application/json",	
+		data: data,
 		beforeSend: function(xhr){
 			xhr.setRequestHeader("Authorization", "Token "+ window.localStorage["token"])
 		},
 		success: function(response){
-			console.log(response);
-			$("#edit_exp_cat").empty();
-			$("#edit_exp_title").empty();
-			$("#edit_exp_des").empty();
-			$("#edit_exp_cost").empty();
-			for(let i of response){
-				console.log(i)
-				$("#edit_exp_cat").append(`
-					<label for="recipient-name" class="col-form-label">${i.categories.title}</label>
-					<br>
-					`)
+			$("#edit_exp_div").empty();
+			$("#edit_exp_div").append(`
+				<div id="edit_exp_div_item">
 
-				$("#edit_exp_title").append(`
-					<input class="form-control" value="${i.title}">
-					<br>
+				</div>	
+				`)
+			
+			$("#edit_exp_div_item").append(`
+				<table  class="table" id="edit_exp_div_item_table">
+				<thead>
+						<tr">
+						<td scope="col"><b>Category</b></td>
+						<td scope="col"><b>Title</b></td>
+						<td scope="col"><b>Description</b></td>
+						<td scope="col"><b>Cost</b></td>
+						<td scope="col"><b>Option</b></td>
+						</tr>
+				</thead>
+				</table>
+				`)
+			edit_exp_id = 1;
+			for(let i of response){
+				
+				$("#edit_exp_div_item_table").append(`
+						
+						
+						<tr scope="row">
+						<td scope="col">${i.categories.title}</td>
+						<td scope="col"><input class="form-control" id="${edit_exp_id}title" value="${i.title}"></td>
+						<td scope="col"><input class="form-control" id="${edit_exp_id}des" value="${i.description}"></td>
+						<td scope="col"><input class="form-control" id="${edit_exp_id}cost" value="${i.cost}"></td>
+						<td scope="col"><button class="btn btn-sm btn-success"  data-dismiss="modal" onclick="edit_exp_function(${edit_exp_id},${i.id})" style="margin-top:3px;">save</button></td>
+						</tr>					
+					
 					`)
-				$("#edit_exp_des").append(`
-					<input class="form-control" value="${i.description}">
-					<br>
-					`)
-				$("#edit_exp_cost").append(`
-					<input class="form-control" value="${i.cost}">
-					<br>
-					`)
+				edit_exp_id = edit_exp_id + 1;
 			
 			}			
 		}
 	})
 
-});
+};
 
 
+// for after editing the expense and clicking on the save button.
+function edit_exp_function(id,expense_id){
+	title_id = "#"+id+"title"
+	des_id = "#"+id+"des";
+	cost_id = "#"+id+"cost"
+	
+	var title = ($(title_id).val());
+	var des = ($(des_id).val());
+	var cost = ($(cost_id).val());
+	let json_data = {}
+	
+	json_data["title"] = title;
+	json_data["description"] = des;
+	json_data["cost"] = cost;
 
-$("#edit_income").on("click", function(){
+	url = "/api/expense/"+expense_id+"/"
+	$.ajax({
+
+		method:"PATCH",
+		url: url,
+		type: "application/json",
+		data: json_data,
+		beforeSend: function(xhr){
+			xhr.setRequestHeader("Authorization", "Token "+ window.localStorage["token"])
+		},
+		success: function(response){
+			console.log("done");
+		}
+
+	})
+	
+}
+
+
+function edit_income(month_number){
 	
 
+	if(income_date + month_number <= 1){
+		income_date = 1;
+	}
+	else if(income_date + month_number >= 12){
+		income_date = 12
+	}
+	else{
+
+		income_date = month_number + income_date
+	}
+
+	console.log(income_date);
 	$.ajax({
 
 		method:"GET",
 		url:"/api/income/",
 		type:"application/json",
+		data: {"month":4},
+		beforeSend: function(xhr){
+			xhr.setRequestHeader("Authorization", "Token "+ window.localStorage["token"])
+		},
+		
+		success: function(response){
+			$("#edit_inc_div").empty();
+			$("#edit_inc_div").append(`
+				<div id="edit_inc_div_item">
+
+				</div>	
+				`)
+			
+			$("#edit_inc_div_item").append(`
+				<table  class="table" id="edit_inc_div_item_table">
+				<thead>
+						<tr">
+						<td scope="col"><b>Category</b></td>
+						<td scope="col"><b>Title</b></td>
+						<td scope="col"><b>Description</b></td>
+						<td scope="col"><b>Cost</b></td>
+						<td scope="col"><b>Option</b></td>
+						</tr>
+				</thead>
+				</table>
+				`)
+			edit_inc_id = 1;
+			for(let i of response){
+				
+				$("#edit_inc_div_item_table").append(`
+						
+						
+						<tr scope="row">
+						<td scope="col">${i.categories.title}</td>
+						<td scope="col"><input class="form-control" id="${edit_inc_id}titlei" value="${i.title}"></td>
+						<td scope="col"><input class="form-control" id="${edit_inc_id}desi" value="${i.description}"></td>
+						<td scope="col"><input class="form-control" id="${edit_inc_id}costi" value="${i.money}"></td>
+						<td scope="col"><button class="btn btn-sm btn-success"  data-dismiss="modal" onclick="edit_inc_function(${edit_inc_id},${i.id})" style="margin-top:3px;">save</button></td>
+						</tr>					
+					
+					`)
+				edit_inc_id = edit_inc_id + 1;
+			
+			}			
+		}			
+		
+	})
+
+};
+
+
+function edit_inc_function(id,income_id){
+	title_id = "#"+id+"titlei"
+	des_id = "#"+id+"desi";
+	cost_id = "#"+id+"costi"
+	
+	var title = ($(title_id).val());
+	var des = ($(des_id).val());
+	var cost = ($(cost_id).val());
+	let json_data = {}
+	
+	json_data["title"] = title;
+	json_data["description"] = des;
+	json_data["money"] = cost;
+
+	console.log(json_data);
+	url = "/api/income/"+income_id+"/"
+	$.ajax({
+
+		method:"PATCH",
+		url: url,
+		type: "application/json",
+		data: json_data,
 		beforeSend: function(xhr){
 			xhr.setRequestHeader("Authorization", "Token "+ window.localStorage["token"])
 		},
 		success: function(response){
-			console.log(response);
-			$("#edit_inc_cat").empty();
-			$("#edit_inc_title").empty();
-			$("#edit_inc_des").empty();
-			$("#edit_inc_cost").empty();
-			for(let i of response){
-				console.log(i)
-				
-				$("#edit_inc_cat").append(`
-					<label for="recipient-name" class="col-form-label">${i.categories.title}</label>
-					<br>
-					`)
-
-				$("#edit_inc_title").append(`
-					<input class="form-control" value="${i.title}">
-					<br>
-					`)
-				$("#edit_inc_des").append(`
-					<input class="form-control" value="${i.description}">
-					<br>
-					`)
-				$("#edit_inc_cost").append(`
-					<input class="form-control" value="${i.cost}">
-					<br>
-					`)
-			
-			}			
+			console.log("done");
 		}
+
 	})
-
-});
-
-
+	
+}
 
 
 
