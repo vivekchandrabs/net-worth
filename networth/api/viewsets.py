0e54c4be74807cbb1985841a2ef2b90cd70e1431
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-23 17:56:56 +0530

    modify the expense and income viewset

--- networth/api/viewsets.py
+++ networth/api/viewsets.py
@@ -8,15 +8,23 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.decorators import login_required
 from rest_framework.authtoken.models import Token
 import json
+import datetime
 
 #for the add category button and the delete catgory button.
 class CategoryViewSet(viewsets.ModelViewSet):
-
+	'''
+	This viewset is for manageing the categories 
+	Add, Delete, update category can be done.
+	'''
 	queryset = Category.objects.all()
 	serializer_class = CategorySerializer
 	permission_classes = (IsAuthenticated,)
 
 	def create(self,request):
+		'''
+		:method:POST:
+		creates a new category.
+		'''
 
 		user = request.user
 		print(user)
@@ -33,6 +41,10 @@ class CategoryViewSet(viewsets.ModelViewSet):
 		return Response(category.data)
 
 	def get_queryset(self):
+		'''
+		method:GET:
+		returns the list of categories.
+		'''
 
 		user = self.request.user
 		categories = Category.objects.filter(user = user)
@@ -41,24 +53,36 @@ class CategoryViewSet(viewsets.ModelViewSet):
 #for the add expense button
 class ExpenseViewSet(viewsets.ModelViewSet):
 
+	'''
+	This viewset is for managing the expense.
+	serializer: ExpenseSerializer
+	permission_classes IsAuthenticated.
+	'''
+
 	queryset = Expense.objects.all()
 	serializer_class = ExpenseSerializer
 	permission_classes = [IsAuthenticated,]
 
 	def create(self,request):
+		'''
+		:method:POST:
+		creating the new expense.
+		'''
+
 		user = request.user
 		print(request.data)
 		title = request.data['title']
 		description = request.data['description']
 		cost = request.data['cost']
 		category_pk = request.data['categories']
-	
+		
 		category_instance = Category.objects.get(pk = category_pk)
 		expense = Expense(categories = category_instance,
 							title = title,
 							description = description,
 							cost = cost,
-							user = user
+							user = user,
+							timestamp = datetime.datetime.now()
 							)
 		expense.save()
 		ex = ExpenseSerializer(expense)
@@ -66,20 +90,33 @@ class ExpenseViewSet(viewsets.ModelViewSet):
 
 
 	def get_queryset(self):
+		'''
+		:method:GET:
+		returns a list of expenses according to the month given.
+		'''
 		user = self.request.user
 		print(self.request.data)
-		month = self.request.data["month"]
+		month = self.request.GET.get("month")
 		expense = Expense.objects.filter(user = user,timestamp__month=month)
 		return expense
 
 #for the add income button.
 class IncomeViewSet(viewsets.ModelViewSet):
-
+	'''
+	This view set is for the managing the income.
+	add, delete, update income.
+	serializer: IncomeSerializer
+	permission_classes: IsAuthenticated.
+	'''
 	queryset = Income.objects.all()
 	serializer_class = IncomeSerializer
 	permission_classes = [IsAuthenticated,]
 
 	def create(self,request):
+		'''
+		:method:POST:
+		for creating a new income.
+		'''
 		user = request.user
 		title = request.data['title']
 		description = request.data['description']
@@ -91,14 +128,19 @@ class IncomeViewSet(viewsets.ModelViewSet):
 						title = title,
 						description = description,
 						money = money,
-						user = user)
+						user = user,
+						timestamp = datetime.datetime.now())
 		income.save()
 		income = IncomeSerializer(income)
 		return Response(income.data)
 	
 	def get_queryset(self):
+		'''
+		:method:GET:
+		this returns a list of incomes of the user according to the month
+		'''
 		user = self.request.user
-		month = self.request.data['month']
+		month = self.request.GET.get('month')
 		income = Income.objects.filter(user = user,timestamp__month=month)
 		return income
 
@@ -114,10 +156,18 @@ class MonthExpenseViewSet(viewsets.ModelViewSet):
 		return query
 
 class SignupViewSet(viewsets.ModelViewSet):
+	'''
+	This is for the creating a new user.
+	permission_classes: AllowAny
+	'''
 
 	permission_classes = (AllowAny,)
 
 	def create(self,request):
+		'''
+		:method:POST:
+		To create a new user.
+		'''
 		username = request.data['username']
 		password = request.data['password']
 		email = request.data['email']
@@ -129,10 +179,18 @@ class SignupViewSet(viewsets.ModelViewSet):
 
 #for pie chart data in dash board
 class AllMonthViewSet(viewsets.ViewSet):
+	'''
+	This is for returning the list of expense.
+	permission_classes: IsAuthenticated.
+	'''
+
 	permission_classes = (IsAuthenticated,)
 
 	def list(self, request):
-
+		'''
+		:method:GET,
+		returns the list of expenses according to the year.
+		'''
 		data = {}
 		user = request.user
 		expense = Expense.objects.filter(timestamp__year=2019, user = user)
@@ -157,6 +215,9 @@ class AllMonthViewSet(viewsets.ViewSet):
 
 #for bar chart data in dash board
 class IncomeExpenseViewSet(viewsets.ViewSet):
+	'''
+	This returns the list of income and expenses of the part
+	'''
 	permissions_classes = (IsAuthenticated,)
 
 	def list(self, request):
