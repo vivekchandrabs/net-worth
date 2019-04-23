bfadbe022be4938bafe941a37f4754f11fabf721
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-23 19:14:13 +0530

    add base_name

--- networth/api/urls.py
+++ networth/api/urls.py
@@ -6,7 +6,7 @@ from api.viewsets import *
 router = DefaultRouter()
 router.register(r'month', MonthExpenseViewSet, base_name="MonthExpense")
 router.register(r'category',CategoryViewSet)
-router.register(r'expense',ExpenseViewSet)
+router.register(r'expense',ExpenseViewSet, base_name="expense_list")
 router.register(r'income',IncomeViewSet)
 router.register(r'signup',SignupViewSet, base_name="Signup")
 router.register(r'allmonthexpense',AllMonthViewSet, base_name="AllMonth")
